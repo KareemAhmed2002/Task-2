5c6e8330b3b39d17cb951972d00a23fbf4900f43
Kareem ahmed <126814845+KareemAhmed2002@users.noreply.github.com>
2023-03-19 02:06:14 +0200

    Update sketch_mar18a.ino

--- Ping Pong effect/sketch_mar18a/sketch_mar18a.ino
+++ Ping Pong effect/sketch_mar18a/sketch_mar18a.ino
@@ -1,51 +1,50 @@
 void setup() {
-  // set pins 2 to 6 as outputs
+ 
   for (int i = 2; i <= 6; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
 void loop() {
-  // turn on the first LED
+  
   digitalWrite(2, HIGH);
   delay(100);
   
-  // turn off the first LED and turn on the second LED
+ 
   digitalWrite(2, LOW);
   digitalWrite(3, HIGH);
   delay(100);
   
-  // turn off the second LED and turn on the third LED
+  
   digitalWrite(3, LOW);
   digitalWrite(4, HIGH);
   delay(100);
   
-  // turn off the third LED and turn on the fourth LED
   digitalWrite(4, LOW);
   digitalWrite(5, HIGH);
   delay(100);
   
-  // turn off the fourth LED and turn on the fifth LED
+  
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
   delay(100);
   
-  // turn off the fifth LED and turn on the fourth LED
+  
   digitalWrite(6, LOW);
   digitalWrite(5, HIGH);
   delay(100);
   
-  // turn off the fourth LED and turn on the third LED
+  
   digitalWrite(5, LOW);
   digitalWrite(4, HIGH);
   delay(100);
   
-  // turn off the third LED and turn on the second LED
+  
   digitalWrite(4, LOW);
   digitalWrite(3, HIGH);
   delay(100);
   
-  // turn off the second LED and turn on the first LED
+  
   digitalWrite(3, LOW);
   digitalWrite(2, HIGH);
   delay(100);
