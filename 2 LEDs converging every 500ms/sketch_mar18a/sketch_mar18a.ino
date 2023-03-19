5749ddf3ecbd4f72c2f2fe1223f90bc7489dd132
Kareem ahmed <126814845+KareemAhmed2002@users.noreply.github.com>
2023-03-19 02:03:12 +0200

    Update sketch_mar18a.ino

--- 2 LEDs converging every 500ms/sketch_mar18a/sketch_mar18a.ino
+++ 2 LEDs converging every 500ms/sketch_mar18a/sketch_mar18a.ino
@@ -1,16 +1,15 @@
-void setup() {
-  // set pins 2 and 3 as outputs
+void setup(){
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
 }
 
 void loop() {
-  // turn on LED 1 and turn off LED 2
+  
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
   delay(500);
   
-  // turn off LED 1 and turn on LED 2
+ 
   digitalWrite(2, LOW);
   digitalWrite(3, HIGH);
   delay(500);
