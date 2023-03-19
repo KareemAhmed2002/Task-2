4e1fee156388e971cbb66b4764c5b949672c3996
Kareem ahmed <126814845+KareemAhmed2002@users.noreply.github.com>
2023-03-19 02:09:09 +0200

    Update sketch_mar18b.ino

--- Snake effect/sketch_mar18b/sketch_mar18b.ino
+++ Snake effect/sketch_mar18b/sketch_mar18b.ino
@@ -1,17 +1,16 @@
 void setup() {
-  // set pins 2 to 6 as outputs
+ 
   for (int i = 2; i <= 6; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
 void loop() {
-  // turn on the first LED and turn off the last LED
+ 
   digitalWrite(2, HIGH);
   digitalWrite(6, LOW);
   delay(100);
   
-  // turn off the first LED and turn on the second LED
  digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, LOW);
@@ -19,7 +18,7 @@ void loop() {
   digitalWrite(6, LOW);
   delay(100);
   
-  // turn off the second LED and turn on the third LED
+ 
  digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
@@ -27,7 +26,7 @@ void loop() {
   digitalWrite(6, LOW);
   delay(100);
   
-  // turn off the third LED and turn on the fourth LED
+  
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
@@ -35,15 +34,14 @@ void loop() {
   digitalWrite(6, LOW);
   delay(100);
   
-  // turn off the fourth LED and turn on the last LED
+  
    digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(6, HIGH);
   delay(100);
-  
-  // turn off the last LED and turn on the fourth LED
+ 
     digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
@@ -51,7 +49,6 @@ void loop() {
   digitalWrite(6, HIGH);
   delay(100);
   
-  // turn off the fourth LED and turn on the third LED
     digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
@@ -59,7 +56,7 @@ void loop() {
   digitalWrite(6, HIGH);
   delay(100);
   
-  // turn off the third LED and turn on the second LED
+  
      digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, HIGH);
@@ -67,7 +64,7 @@ void loop() {
   digitalWrite(6, HIGH);
   delay(100);
   
-  // turn off the second LED and turn on the first LED
+ 
    digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
